2664d1f116ff7ebb42d86e71a3dbd27d95db75aa
iamliuzy <liuzhiyu.sh@outlook.com>
2024-03-17 00:00:03 +0800

    A lot of changes
    I tried to get it to work on Android TV but now for some reason it can't even do the most basic compilation :(

--- app/src/main/java/xyz/yingshaoxo/android/subtitlekiller/MainActivity.kt
+++ app/src/main/java/xyz/yingshaoxo/android/subtitlekiller/MainActivity.kt
@@ -6,11 +6,13 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.os.Handler
+import android.view.KeyEvent
 import android.widget.Toast
 import com.linchaolong.android.floatingpermissioncompat.FloatingPermissionCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import xyz.yingshaoxo.android.subtitlekiller.GlobalVariable.service_intent
 import xyz.yingshaoxo.android.subtitlekiller.GlobalVariable.started
+import xyz.yingshaoxo.android.subtitlekiller.FloatingAppService.*
 
 
 object GlobalVariable {
@@ -68,6 +70,25 @@ class MainActivity : AppCompatActivity() {
         textView.setOnLongClickListener {
             stop_service()
             finish()
+            true
+        }
+
+        textView.setOnKeyListerer { view, code, event ->
+            if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                when (code) {
+                    KeyEvent.KEYCODE_DPAD_UP -> FloatingAppService.params.y = FloatingAppService.initial_layout_y - 1  //Up
+                    KeyEvent.KEYCODE_DPAD_DOWN -> FloatingAppService.params.y = FloatingAppService.initial_layout_y + 1 //Down
+                    KeyEvent.KEYCODE_DPAD_LEFT -> FloatingAppService.params.height -= 1  //Left
+                    KeyEvent.KEYCODE_DPAD_RIGHT -> FloatingAppService.params.height += 1  //Right
+                }
+                if (FloatingAppService.params.height < FloatingAppService.min_height) {
+                    FloatingAppService.params.height = FloatingAppService.min_height
+                }
+                if (FloatingAppService.params.height > FloatingAppService.max_height) {
+                    FloatingAppService.params.height = FloatingAppService.min_height
+                }
+                FloatingAppService.window_manager.updateViewLayout(FloatingAppService.myView, FloatingAppService.params)
+            }
             true
         }
 
